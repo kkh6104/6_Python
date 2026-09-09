5e3f8ba74109c2f7ffadc8dec10044d9ffdfbbd3
GyoungHwan Kwon <kkh6104@gmail.com>
2026-09-09 11:43:22 +0900

    26.09.09 수업

--- 02_data_structures/01_list.py
+++ 02_data_structures/01_list.py
@@ -24,7 +24,7 @@ print(f"temp --> {bool(temp)}")
 
 print("=" * 60)
 items = ["에이스", "아이비", "코피코"]
-
+    
 print(f"items --> {items}")
 
 # 데이터 추가 : append(), insert(), extend()
@@ -51,3 +51,51 @@ print(f"pop - 맨 뒤에 데이터를 삭제 후 반환: {snack} / {items}")
 
 del items[0]
 print(f"del - 인덱스로 삭제: {items}")
+
+print("=" * 60)
+# 탐색, 정보 조회
+numbers = [5, 1, 3, 2, 7, 2]
+
+# 찾을값 in 리스트 => 값이 있으면 True, 없으면 False
+# 리스트.index(찾을값) => 값이 있으면 해당 인덱스, 없으면 오류 발생
+
+print(f"numbers 에 7이 있는지 ? {7 in numbers}")
+print(f"numbers 에 7이 있는지 ? {numbers.index(7)}")
+
+print(f"numbers 에 9이 있는지 ? {9 in numbers}")
+# print(f"numbers 에 9이 있는지 ? {numbers.index(9)}")
+
+print(f"numbers 에서 2의 개수 : {numbers.count(2)}")
+print(f"numbers 에서 9의 개수 : {numbers.count(9)}")
+
+print(f"리스트 길이: {len(numbers)}")
+
+print(f"{numbers}")
+numbers.sort()      # 해당 리스트의 값을 변경
+print(f"sort -> {numbers}")
+numbers.sort(reverse = True)
+print(f"sort(reverse = True) -> {numbers}")
+
+fruits = ["banana", "cherry", "apple"]
+fruits.sort()
+print(f"문자열 정렬 -> {fruits}")
+fruits.reverse()        # 해당 리스트를 역순으로 변경
+print(f"reverse() -> {fruits}")
+
+print("="*60)
+
+# 2차원 리스트
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+print(f"1, 1 -> {matrix[1][1]}")
+print()
+
+for row in matrix:
+    # print(row)
+    for value in row:
+        print(value, end=" ")
+    print()
