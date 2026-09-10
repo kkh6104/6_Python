084e48b910246ac78f13b3a6df89c94e0071329b
GyoungHwan Kwon <kkh6104@gmail.com>
2026-09-10 16:03:49 +0900

    26.09.10 수업

--- practice/aiselection.py
+++ practice/aiselection.py
@@ -125,10 +125,16 @@ employees = {
     "정우성": ("마케팅팀", 320)
 }
 def analyze_employees(employees):
-    max(employees, key=lambda x : x.items()[1][1])
-    min(employees, key=lambda x : x.items()[1][1])
-    
+    h = max(employees.items(), key=lambda x : x[1][1])
+    l = min(employees.items(), key=lambda x : x[1][1])
+    avg = sum(x[1] for x in employees.values()) / len(employees)
+    return (h[0],h[1][1]), (l[0],l[1][1]), avg
 
+h,l,avg = analyze_employees(employees)
+print("========== 직원 월급 통계 ==========")
+print(f"- 최고 연봉자: {h[0]} ({h[1]}만원)")
+print(f"- 최저 연봉자: {l[0]} ({l[1]}만원)")
+print(f"- 평균 월급: {avg:.1f}만원")
 
 
 
@@ -177,4 +183,48 @@ user.balance = 20000
 print(user.balance) # 20000
 
 user.balance = -5000 # 잔액은 0원 이상이어야 합니다.
+"""
+class BankUser:
+    def __init__(self, name, balance = 0):
+        self.name = name
+        self.__balance = balance
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    @balance.setter
+    def balance(self, value):
+        if value < 0:
+            print("잔액은 0원 이상이어야 합니다.")
+            return
+        self.__balance = value
+
+    def __str__(self):
+        return f"[{self.name}] 님의 잔액: [{self.__balance}]원"
+
+user = BankUser("권경환", 10000)
+print(user)
+
+user.balance = 20000
+print(user.balance)
+
+user.balance = -5000
+
+"""
+파이썬 OOP 종합 퀴즈 & 실습 문제
+[문제 1] 쇼핑몰 장바구니 클래스 (__len__, __add__, __str__ 구현)
+상품(Item) 객체들을 장바구니(Cart)에 담고, 던더 메소드를 활용해 수량 파악 및 장바구니 병합 기능을 구현하세요.
+
+요구사항:
+
+Item 클래스는 name과 price를 필드로 가집니다.
+
+Cart 클래스는 내부 리스트(self.items)에 Item들을 저장합니다.
+
+len(cart) 호출 시 장바구니에 담긴 전체 상품 개수를 반환하도록 __len__을 구현하세요.
+
+cart1 + cart2 연산 시 두 장바구니의 상품이 합쳐진 새로운 Cart 객체를 반환하도록 __add__를 구현하세요.
+
+print(cart) 호출 시 "장바구니 총 X개 상품 (총금액: Y원)" 형태로 출력되도록 __str__을 구현하세요.
 """
